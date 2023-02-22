af8414bd3773e8c12b976b8e453fccc934c9495e
Anthony Rezzonico <anthonyrezz@gmail.com>
2023-02-22 14:47:15 -0700

    Inital commit

---
+++ src/main/java/com/avrezzon/sb3_nativeimage/Sb3NativeImageApplication.java
@@ -0,0 +1,13 @@
+package com.avrezzon.sb3_nativeimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3NativeImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb3NativeImageApplication.class, args);
+	}
+
+}
